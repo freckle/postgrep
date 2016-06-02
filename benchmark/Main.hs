5b8b983e318999aa8165540e952219061630dfba
David Reaver <johndreaver@gmail.com>
2016-06-02 10:26:22 -0700

    Added a benchmark

---
+++ benchmark/Main.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Criterion.Main
+
+import Data.List (intersperse)
+import qualified Data.Text as T
+
+import PostGrep
+
+setupEnv :: IO ([T.Text], [T.Text], [T.Text])
+setupEnv = do
+  let single = shortEntry
+      small = concat $ take 1000 $ intersperse longEntry (repeat shortEntry)
+      large = concat $ take 100000 $ intersperse longEntry (repeat shortEntry)
+  return (single, small, large)
+
+shortEntry :: [T.Text]
+shortEntry =
+  ["2016-05-19 18:44:03 UTC:10.0.120.51(52251):classroom_api@classroom_prod:[7652]:LOG:  duration: 0.013 ms  statement: BEGIN"]
+
+longEntry :: [T.Text]
+longEntry =
+  [ "2016-05-19 18:44:15 UTC:10.0.120.51(52094):datadog@classroom_prod:[6580]:LOG:  duration: 10.753 ms  statement:"
+  , "SELECT schemaname, count(*)"
+  , "FROM pg_stat_user_tables"
+  , "GROUP BY schemaname"
+  ]
+
+rdsParser :: LogLineParser
+rdsParser = logLineParser rdsPrefix
+
+main :: IO ()
+main = defaultMain [
+   -- notice the lazy pattern match here!
+   env setupEnv $ \ ~(single, small, large) -> bgroup "main"
+     [ bench "parseLines single" $ nf length (parseLogLines rdsParser single)
+     , bench "parseLines small" $ nf length (parseLogLines rdsParser small)
+     , bench "parseLines large" $ nf length (parseLogLines rdsParser large)
+     ]
+   ]
