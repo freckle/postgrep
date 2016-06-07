57f2574a00ec0f7da640f5cd1d20a37a7f3e0bce
David Reaver <johndreaver@gmail.com>
2016-06-06 18:30:01 -0700

    Fix the conduit concaetating all lines

--- src/PostGrep/Conduit.hs
+++ src/PostGrep/Conduit.hs
@@ -7,7 +7,6 @@ module PostGrep.Conduit
 
 import qualified Data.ByteString as BS
 import Data.Conduit
-import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as CL
 
 import PostGrep.LogEntry
@@ -15,7 +14,6 @@ import PostGrep.LogLine
 
 logConduit :: (Monad m) => LogLineParser -> ConduitM BS.ByteString LogEntry m ()
 logConduit parser =
-  CB.lines $=
   CL.map (parseLogLine parser) $=
   CL.groupBy groupParsedLines $=
   CL.map (concatMap fst) $=
